cdaefb8fed42cfdffbac31479c009196aa3304cb
Esanpons <ecs200@hotmail.com>
2023-07-01 16:17:31 +0200

    Crear Un JavaScript Para Los Ejercicios

---
+++ Ejercicios .js
@@ -0,0 +1,15 @@
+var NombreDeUsuario
+var Seguridad
+//Intro
+function NombreDeUsuarioF(NombreDeUsuario) {
+    var NombreDeUsuario = prompt('Que Nombre de Usuario Quieres Ponerte')
+    alert('Hola ' + NombreDeUsuario)
+    alert('Encantado de Conocerte ' + NombreDeUsuario)
+    alert('Seguro Que Lo Pasaremos Muy Bien ' + NombreDeUsuario)
+    alert('Ves A Tu Primer Ejercicio ' + NombreDeUsuario)
+}
+//1
+function Ejercicio() {
+    alert('Vamos A Acer Paragrafos')
+    alert('<p> </p>')
+}
