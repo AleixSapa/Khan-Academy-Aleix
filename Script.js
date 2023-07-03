c6eed839478e971695493b045bd1e6ceec8dd476
AleixSaPa <sapafamilia@gmail.com>
2023-07-03 19:45:01 +0200

    para cargar

---
+++ Script.js
@@ -0,0 +1,4 @@
+function Ejercicio(Num) {
+    if (Num === 1) { alert('Uoooo Vas A Empecar   Vamos A El Egercicio Numero 1') }
+    else if (Num === 2) { alert('Uoooo Vas A Empecar   Vamos A El Egercicio Numero 2') }
+}
